35ff666b9146c2a3390aa410f3ec6a27dde7041c
kendry Finol <kendryfbo@gmail.com>
2016-11-29 15:10:56 -0400

    Mejora en la pagina index

--- html/index.php
+++ html/index.php
@@ -18,15 +18,14 @@ else if (isset($_POST['portfolio']))
 <?php require("../view/header.php"); ?>
 
 <div class="container text-xs-center">
-  <h1>Welcome to CS75 Appliance</h1>
+  <h1>Welcome to CS75 Finances</h1>
 </div>
 <div class="container text-xs-center">
   <spam>
-    <i class="fa fa-certificate fa-5x bg-active"></i>
-    <i class="fa fa-certificate fa-5x bg-active"></i>
-    <i class="fa fa-certificate fa-5x bg-active"></i>
-    <i class="fa fa-certificate fa-5x bg-inactive"></i>
-    <i class="fa fa-certificate fa-5x bg-inactive"></i>
+    <i class="fa fa-usd fa-5x bg-active"></i>
+    <i class="fa fa-usd fa-5x bg-inactive"></i>
+    <i class="fa fa-usd fa-5x bg-active"></i>
+    <i class="fa fa-usd fa-5x bg-inactive"></i>
   <spam>
 </div>
 <?php require("../view/footer.php"); ?>
